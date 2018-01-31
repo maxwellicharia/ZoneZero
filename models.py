6859f8449aca5f494224f74b8de6926d106b2e35
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-01-31 21:54:57 +0300

    [Edit] - Database connectivity

--- models.py
+++ models.py
@@ -2,41 +2,44 @@ import os
 import urlparse
 import psycopg2
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+class Models:
 
+    def __init__(self):
+        pass
 
-def create(self):
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-    self.con.execute('CREATE TABLE Notes IF NOT EXISTS(NoteID INT AUTOINCREMENT, '
-                     'NoteDate DATE, NoteName TEXT, NoteContent TEXT)')
-    self.con.close()
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
 
+    cur = conn.cursor()
 
-def extract(self):
-    pass
+    def create(self):
 
+        self.cur.execute('CREATE TABLE Notes IF NOT EXISTS(NoteID INT AUTOINCREMENT, '
+                         'NoteSubject TEXT, NoteName TEXT, NoteContent TEXT)')
+        self.cur.close()
 
-def insert(self, date, name, content):
-    self.con.execute('INSERT INTO Notes(NoteDate, NoteName, NoteContent) VALUES(?, ?, ?)', (date, name, content))
-    self.con.close()
+    def extract(self):
+        pass
 
+    def insert(self, date, name, content):
+        self.cur.execute('INSERT INTO Notes(NoteDate, NoteName, NoteContent) VALUES(?, ?, ?)', (date, name, content))
+        self.cur.close()
 
-def edit(self, date, name, content):
-    pass
+    def edit(self, date, name, content):
 
+        pass
 
-def delete(date, name):
-    pass
+    def delete(date, name):
+        pass
 
-
-def view(date, name):
-    pass
+    def view(date, name):
+        pass
