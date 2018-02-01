56e410562c033055179058187bd214336cabdc37
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-01 21:32:26 +0300

    [Add] - Database Methods

--- models.py
+++ models.py
@@ -1,38 +1,25 @@
-import os
-import urlparse
-import psycopg2
-
-
 class Models:
 
     def __init__(self):
         pass
 
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    # with app.app.context()
+    #     mysql = MySQL()
 
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port
-    )
-
-    cur = conn.cursor()
 
     def create(self):
-
-        self.cur.execute('CREATE TABLE Notes IF NOT EXISTS(NoteID INT AUTOINCREMENT, '
-                         'NoteSubject TEXT, NoteName TEXT, NoteContent TEXT)')
-        self.cur.close()
+        # self.cur.execute('CREATE TABLE Notes IF NOT EXISTS(NoteID INT AUTOINCREMENT, '
+        #                  'NoteSubject TEXT, NoteName TEXT, NoteContent TEXT)')
+        # self.cur.close()
+        pass
 
     def extract(self):
         pass
 
     def insert(self, date, name, content):
-        self.cur.execute('INSERT INTO Notes(NoteDate, NoteName, NoteContent) VALUES(?, ?, ?)', (date, name, content))
-        self.cur.close()
+        # self.cur.execute('INSERT INTO Notes(NoteDate, NoteName, NoteContent) VALUES(?, ?, ?)', (date, name, content))
+        # self.cur.close()
+        pass
 
     def edit(self, date, name, content):
 
