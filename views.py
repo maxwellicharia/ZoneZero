bde39b322f468cc0c22519ff6a8724da6f4a76e5
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-02 17:05:00 +0300

    [Add] - View Functions

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import ssl
+
+from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap
 from flask_cas import CAS, login_required
 
@@ -9,10 +11,15 @@ from models import Models
 
 app = Flask(__name__)
 Bootstrap(app)
-CAS(app)
 
-app.config['CAS_SERVER'] = 'maxwell-Lenovo-YOGA-510-14ISK:8080/cas/login'
-app.config['CAS_AFTER_LOGIN'] = 'dash'
+ssl._create_default_https_context = ssl._create_unverified_context
+
+cas = CAS(app, '/cas')
+
+app.config['CAS_SERVER'] = 'https://127.0.0.1:8443/cas/login/'
+app.config['CAS_AFTER_LOGIN'] = '/dash'
+app.config['CAS_ATTRIBUTES'] = ""
+app.config['CAS_AFTER_LOGOUT'] = '/logout'
 # with app.app_context():
 #     # MySQL configurations
 #     app.config['MYSQL_DATABASE_USER'] = 'notes'
@@ -33,14 +40,30 @@ app.secret_key = "My Secret Key"
 # form=form generates the form required as the HTML file is rendered
 
 
+# @app.route('/logout', methods=['GET', 'POST'])
+# @logout
+# def logout
+#     return render_template('welcome.html')
+
 @app.route('/', methods=['GET', 'POST'])
+def root():
+    return render_template('welcome.html')
+
+
 @login_required
+@app.route('/dash', methods=['GET', 'POST'])
 # root route that the user will see after been authenticated in CAS
 def dash():
-    if request.method == 'GET':
-        return render_template('dash.html')
+    return render_template('dash.html')
 
 
+@login_required
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    return url_for('cas.logout')
+
+
+@login_required
 @app.route('/create', methods=['GET', 'POST'])
 # route that coordinates the creation of a new note
 def create():
@@ -53,25 +76,32 @@ def create():
             return render_template('create.html', form=form)
         else:
             createdb = Models()
-            createdb.create()
+            subject = request.form['notesubject']
+            name = request.form['notename']
+            content = request.form['notecontent']
+            createdb.insert(subject, name, content)
             return render_template('success.html', create=True)
 
 
+@login_required
 @app.route('/update', methods=['GET', 'POST'])
 def update():
     return render_template('update.html')
 
 
+@login_required
 @app.route('/view', methods=['GET', 'POST'])
 def view():
     return render_template('view.html')
 
 
+@login_required
 @app.route('/delete')
 def delete():
     return render_template('delete.html')
 
 
+@login_required
 @app.route('/success', methods=['GET', 'POST'])
 def success():
     return render_template('success.html')
