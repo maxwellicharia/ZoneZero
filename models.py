145b02dc7a207e969c71e32b6764aa2f9e3c2d95
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-03 17:43:24 +0300

    [Add] - DB Functions

--- models.py
+++ models.py
@@ -9,27 +9,38 @@ class Models:
 
     def create(self):
         with self.con:
-            self.cur.execute('CREATE TABLE Notes(NoteID INT, '
-                             'NoteSubject TEXT, NoteName TEXT, NoteContent TEXT)')
-            self.cur.close()
-        pass
-
-    def extract(self):
-        pass
+            try:
+                self.cur.execute("CREATE TABLE IF NOT EXISTS Notes(NoteID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                 "NoteSubject TEXT, NoteName TEXT, NoteContent TEXT)")
+                self.con.commit()
+            except lite.Error, e:
+                print "An Error Occurred: %s" % e
+                self.con.rollback()
 
     def insert(self, subject, name, content):
         with self.con:
-            self.cur.execute('INSERT INTO Notes(NoteSubject, NoteName, NoteContent) VALUES(?, ?, ?)',
-                             (subject, name, content))
-            self.cur.close()
-        pass
+            try:
+                self.cur.execute("INSERT INTO Notes(NoteSubject, NoteName, NoteContent) VALUES(?, ?, ?)",
+                                 (subject, name, content))
+                self.con.commit()
+            except lite.Error, e:
+                self.con.rollback()
+                print "An Error Occurred: %s" % e
 
-    def edit(self, subject, name, content):
+    def edit(self, id):
+        with self.con:
+            pass
 
-        pass
+    def delete(self, id):
+        with self.con:
+            pass
 
-    def delete(subject, name):
-        pass
-
-    def view(subject, name):
-        pass
+    def view(self):
+        with self.con:
+            try:
+                self.cur.execute("SELECT * FROM Notes")
+                rows = self.cur.fetchall()
+                print rows
+                return rows
+            except lite.Error, e:
+                return "Error: " % e
