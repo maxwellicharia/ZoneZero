936a43939d8219d811bbcd01490d6ad8b2271927
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-03 16:57:07 +0300

    [Debug] - Minor Errors

--- views.py
+++ views.py
@@ -19,71 +19,75 @@ app.config['CAS_ATTRIBUTES'] = ""
 app.secret_key = "My Secret Key"
 
 
-class Routes:
-    def __init__(self):
-        pass
+# the methods GET and POST are used to determine if content is received or being submitted
 
-    # the methods GET and POST are used to determine if content is received or being submitted
+# render_template returns the HTML file that has been defined
+# in the templates folder that is automatically detected
 
-    # render_template returns the HTML file that has been defined
-    # in the templates folder that is automatically detected
+# form=form generates the form required as the HTML file is rendered
 
-    # form=form generates the form required as the HTML file is rendered
 
-    @app.route('/', methods=['GET', 'POST'])
-    def root():
-        return render_template('welcome.html', norm=True)
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    return render_template('welcome.html', norm=True)
 
-    @app.route('/cas/login', methods=['GET', 'POST'])
-    def login():
-        pass
 
-    @login_required
-    @app.route('/cas/logout', methods=['GET', 'POST'])
-    def logout():
-        pass
+@app.route('/cas/login', methods=['GET', 'POST'])
+def login():
+    pass
 
-    @login_required
-    @app.route('/dash', methods=['GET', 'POST'])
-    # root route that the user will see after been authenticated in CAS
-    def dash():
-        return render_template('dash.html')
 
-    @login_required
-    @app.route('/create', methods=['GET', 'POST'])
-    # route that coordinates the creation of a new note
-    def create():
-        form = Form(request.form)  # instantiating form to use the forms defined from the Form class in form.py
+@login_required
+@app.route('/cas/logout', methods=['GET', 'POST'])
+def logout():
+    pass
 
-        if request.method == 'GET':
+
+@login_required
+@app.route('/dash', methods=['GET', 'POST'])
+# root route that the user will see after been authenticated in CAS
+def dash():
+    return render_template('dash.html')
+
+
+@login_required
+@app.route('/create', methods=['GET', 'POST'])
+# route that coordinates the creation of a new note
+def create():
+    form = Form(request.form)  # instantiating form to use the forms defined from the Form class in form.py
+
+    if request.method == 'GET':
+        return render_template('create.html', form=form)
+    else:
+        if not form.validate_on_submit():  # making sure that the form is validated before submission
             return render_template('create.html', form=form)
         else:
-            if not form.validate_on_submit():  # making sure that the form is validated before submission
-                return render_template('create.html', form=form)
-            else:
-                db = Models()
-                db.create()
+            db = Models()
+            db.create()
 
-                subject = request.form['notesubject']
-                name = request.form['notename']
-                content = request.form['notecontent']
+            subject = request.form['notesubject']
+            name = request.form['notename']
+            content = request.form['notecontent']
 
-                db.insert(subject, name, content)
-                return render_template('success.html', create=True)
+            db.insert(subject, name, content)
+            return render_template('success.html', create=True)
 
-    @login_required
-    @app.route('/update', methods=['GET', 'POST'])
-    def update():
-        return render_template('update.html')
 
-    @login_required
-    @app.route('/view', methods=['GET', 'POST'])
-    def view():
-        db = Models()
-        rows = db.cur.fetchall()
-        return render_template('view.html', rows=rows)
+@login_required
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    return render_template('update.html')
 
-    @login_required
-    @app.route('/success', methods=['GET', 'POST'])
-    def success():
-        return render_template('success.html')
+
+@login_required
+@app.route('/view', methods=['GET', 'POST'])
+def view():
+    db = Models()
+    rows = db.cur.fetchall()
+    return render_template('view.html', rows=rows)
+
+
+@login_required
+@app.route('/success', methods=['GET', 'POST'])
+def success():
+    return render_template('success.html')
