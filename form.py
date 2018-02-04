c9cc23891334a83abc47055805a827041edbb0d2
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-04 14:12:17 +0300

    [Add] - New Form for Update

--- form.py
+++ form.py
@@ -12,5 +12,14 @@ class NoteCreate(FlaskForm):  # class with all the required fields for creating 
     submit = SubmitField("Create Note")
 
 
+class NoteUpdate(FlaskForm):
+    id = IntegerField('Note ID', [validators.DataRequired(message='Note Id needed!')])
+    note_subject = StringField("Note Subject",
+                               [validators.DataRequired(message="Kindly input the Subject of your note")])
+    note_name = StringField("Note Name", [validators.DataRequired(message="Kindly input the name of your note")])
+    note_content = TextAreaField("Note Content", [validators.DataRequired(message="Content required")])
+    submit = SubmitField("Create Note")
+
+
 class NoteID(FlaskForm):
     id = IntegerField('Note ID', [validators.DataRequired(message='Note Id needed!')])
