538e2e6f9cedb4d3e7c4cb589427ae72d626ad3b
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-04 14:14:34 +0300

    [Update] - Views for User

--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_cas import CAS, login_required
 
-from form import Form
+from form import NoteCreate, NoteUpdate, NoteID
 from models import Models
 
 # import necessary modules for use
@@ -18,12 +18,9 @@ app.config['CAS_AFTER_LOGIN'] = 'dash'
 app.config['CAS_ATTRIBUTES'] = ""
 app.secret_key = "My Secret Key"
 
-
 # the methods GET and POST are used to determine if content is received or being submitted
-
 # render_template returns the HTML file that has been defined
 # in the templates folder that is automatically detected
-
 # form=form generates the form required as the HTML file is rendered
 
 
@@ -54,43 +51,65 @@ def dash():
 @app.route('/create', methods=['GET', 'POST'])
 # route that coordinates the creation of a new note
 def create():
-    form = Form(request.form)  # instantiating form to use the forms defined from the Form class in form.py
+    form = NoteCreate(request.form)  # instantiating form to use the forms defined from the Form class in form.py
 
     if request.method == 'GET':
         return render_template('create.html', form=form)
     else:
         if not form.validate_on_submit():  # making sure that the form is validated before submission
-            return render_template('create.html', form=form)
+            return render_template('create.html', form=form, not_validate=True)
         else:
             db = Models()
             db.create()
-
-            subject = request.form['notesubject']
-            name = request.form['notename']
-            content = request.form['notecontent']
-
-            db.insert(subject, name, content)
+            name = request.form['note_name']
+            subject = request.form['note_subject']
+            content = request.form['note_content']
+            db.insert(name, subject, content)
             return render_template('success.html', create=True)
-
-
-@login_required
-@app.route('/update', methods=['GET', 'POST'])
-def update():
-    return render_template('update.html', update=True)
-
-
-@login_required
-@app.route('/delete', methods=['GET', 'POST'])
-def delete():
-    return render_template('update.html', delete=True)
 
 
 @login_required
 @app.route('/view', methods=['GET', 'POST'])
 def view():
     db = Models()
-    rows = db.cur.fetchall()
-    return render_template('view.html', rows=rows)
+    return render_template('view.html', rows=db.view())
+
+
+@login_required
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    form = NoteUpdate(request.form)  # instantiating form to use the forms defined from the Form class in form.py
+    if request.method == 'GET':
+        return render_template('update.html', update=True, form=form)
+    else:
+        if not form.validate_on_submit():  # making sure that the form is validated before submission
+            return render_template('update.html', not_validate=True, form=form)
+        else:
+            db = Models()
+            db.create()
+            num = request.form['id']
+            name = request.form['note_name']
+            subject = request.form['note_subject']
+            content = request.form['note_content']
+            db.update(num, name, subject, content)
+            return render_template('success.html', update=True, form=form)
+
+
+@login_required
+@app.route('/delete', methods=['GET', 'POST'])
+def delete():
+    form = NoteID(request.form)
+    if request.method == 'GET':
+        return render_template('update.html', form=form, delete=True)
+    else:
+        if not form.validate_on_submit():  # making sure that the form is validated before submission
+            return render_template('update.html', form=form, delete=True)
+        else:
+            db = Models()
+            db.create()
+            num = request.form['id']
+            db.delete(num)
+            return render_template('success.html', delete=True)
 
 
 @login_required
