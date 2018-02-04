f380ad0d9d3bc7d1ff48c2a2af601bd35fa5e5f1
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-04 14:13:48 +0300

    [Add] - Models Views for CRUD

--- models.py
+++ models.py
@@ -6,41 +6,52 @@ class Models:
     def __init__(self):
         self.con = lite.connect('notes.db')
         self.cur = self.con.cursor()
+        self.con.text_factory = bytes
 
     def create(self):
         with self.con:
             try:
                 self.cur.execute("CREATE TABLE IF NOT EXISTS Notes(NoteID INTEGER PRIMARY KEY AUTOINCREMENT, "
-                                 "NoteSubject TEXT, NoteName TEXT, NoteContent TEXT)")
+                                 "NoteName TEXT, NoteSubject TEXT, NoteContent TEXT)")
                 self.con.commit()
             except lite.Error, e:
-                print "An Error Occurred: %s" % e
                 self.con.rollback()
+                return e
 
-    def insert(self, subject, name, content):
+    def insert(self, name, subject, content):
         with self.con:
             try:
-                self.cur.execute("INSERT INTO Notes(NoteSubject, NoteName, NoteContent) VALUES(?, ?, ?)",
-                                 (subject, name, content))
+                self.cur.execute("INSERT INTO Notes(NoteName, NoteSubject, NoteContent) VALUES(?, ?, ?)",
+                                 (name, subject, content))
                 self.con.commit()
             except lite.Error, e:
                 self.con.rollback()
-                print "An Error Occurred: %s" % e
-
-    def edit(self, id):
-        with self.con:
-            pass
-
-    def delete(self, id):
-        with self.con:
-            pass
+                return e
 
     def view(self):
         with self.con:
             try:
                 self.cur.execute("SELECT * FROM Notes")
                 rows = self.cur.fetchall()
-                print rows
                 return rows
             except lite.Error, e:
-                return "Error: " % e
+                return e
+
+    def update(self, num, name, subject, content):
+        with self.con:
+            try:
+                self.cur.execute("UPDATE Notes SET NoteName = ?, NoteSubject = ?, NoteContent = ? "
+                                 "WHERE NoteID = ?", (name, subject, content, num))
+                self.con.commit()
+            except lite.Error, e:
+                self.con.rollback()
+                return e
+
+    def delete(self, num):
+        with self.con:
+            try:
+                self.cur.execute("DELETE FROM Notes WHERE NoteID = ?", num)
+                self.con.commit()
+            except lite.Error, e:
+                self.con.rollback()
+                return e
