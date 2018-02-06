dee1e3e6a61e835da5f759a2ecf77939d0657cdc
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-06 22:00:56 +0300

    [Remove] - ID Form Field

--- form.py
+++ form.py
@@ -13,7 +13,6 @@ class NoteCreate(FlaskForm):  # class with all the required fields for creating 
 
 
 class NoteUpdate(FlaskForm):
-    id = IntegerField('Note ID', [validators.DataRequired(message='Note Id needed!')])
     note_subject = StringField("Note Subject",
                                [validators.DataRequired(message="Kindly input the Subject of your note")])
     note_name = StringField("Note Name", [validators.DataRequired(message="Kindly input the name of your note")])
